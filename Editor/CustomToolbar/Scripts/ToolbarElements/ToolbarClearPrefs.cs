953ee2e5bd24e1d7cd6e02b5f06c5681037b7347
Erfan sheikhhoseini <erfan.sheikhhoseini@gmail.com>
2024-11-18 13:37:59 +0330

    delete other save locations

--- Editor/CustomToolbar/Scripts/ToolbarElements/ToolbarClearPrefs.cs
+++ Editor/CustomToolbar/Scripts/ToolbarElements/ToolbarClearPrefs.cs
@@ -1,30 +1,62 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEditor;
 using UnityToolbarExtender;
 
 [Serializable]
 internal class ToolbarClearPrefs : BaseToolbarElement {
-	private static GUIContent clearPlayerPrefsBtn;
+    private static GUIContent clearPlayerPrefsBtn;
 
-	public override string NameInList => "[Button] Clear prefs";
-	public override int SortingGroup => 4;
+    public override string NameInList => "[Button] Clear prefs";
+    public override int SortingGroup => 4;
 
-	public override void Init() {
-		clearPlayerPrefsBtn = EditorGUIUtility.IconContent("SaveFromPlay");
-		clearPlayerPrefsBtn.tooltip = "Clear player prefs";
-	}
 
-	protected override void OnDrawInList(Rect position) {
+    public override void Init() {
+        clearPlayerPrefsBtn = EditorGUIUtility.IconContent("SaveFromPlay");
+        clearPlayerPrefsBtn.tooltip = "Clear player prefs";
+    }
 
-	}
+    protected override void OnDrawInList(Rect position) {
 
-	protected override void OnDrawInToolbar() {
-		if (GUILayout.Button(clearPlayerPrefsBtn, ToolbarStyles.commandButtonStyle)) {
-			PlayerPrefs.DeleteAll();
-			Debug.Log("Clear Player Prefs");
-		}
-	}
+    }
+
+    protected override void OnDrawInToolbar() {
+        if (GUILayout.Button(clearPlayerPrefsBtn, ToolbarStyles.commandButtonStyle)) {
+            PlayerPrefs.DeleteAll();
+            var savePath = $"{Application.persistentDataPath}/StreamingAssets/";
+            var localSavePath = "Assets/StreamingAssets/";
+            ClearSaveDirectory(savePath);
+            ClearSaveDirectory(localSavePath);
+            Debug.Log("Clear Player Prefs");
+        }
+    }
+	
+    private void ClearSaveDirectory(string path)
+    {
+        // Define the directory path
+
+        if (Directory.Exists(path))
+        {
+            // Delete all files in the directory
+            var files = Directory.GetFiles(path);
+            foreach (var file in files)
+            {
+                File.Delete(file);
+            }
+
+            // Delete all subdirectories and their contents
+            var directories = Directory.GetDirectories(path);
+            foreach (var directory in directories)
+            {
+                Directory.Delete(directory, true); // Recursive deletion
+            }
+        }
+        else
+        {
+            Debug.LogWarning($"Directory does not exist: {path}");
+        }
+    }
 }
