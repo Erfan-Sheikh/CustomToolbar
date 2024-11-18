83719d67e090ebebdf37ed503fa842d204093e29
Erfan sheikhhoseini <erfan.sheikhhoseini@gmail.com>
2024-11-18 13:37:37 +0330

    Unity 6 fixes

--- Editor/CustomToolbar/Scripts/ToolbarElements/ToolbarEnterPlayMode.cs
+++ Editor/CustomToolbar/Scripts/ToolbarElements/ToolbarEnterPlayMode.cs
@@ -11,36 +11,37 @@ internal class ToolbarEnterPlayMode : BaseToolbarElement {
 	string[] enterPlayModeOption;
 #endif
 
-	public override string NameInList => "[Dropdown] Enter play mode option";
-	public override int SortingGroup => 2;
+    public override string NameInList => "[Dropdown] Enter play mode option";
+    public override int SortingGroup => 2;
 
-	public override void Init() {
-		enterPlayModeOption = new[]{
-			"Disabled",
-			"Reload All",
-			"Reload Scene",
-			"Reload Domain",
-			"FastMode",
-		};
-	}
+    public override void Init() {
+        enterPlayModeOption = new[]{
+            "Reload All",
+            "Reload Scene",
+            "Reload Domain",
+            "FastMode",
+        };
+    }
 
-	protected override void OnDrawInList(Rect position) {
+    protected override void OnDrawInList(Rect position) {
 
-	}
+    }
 
-	protected override void OnDrawInToolbar() {
+    protected override void OnDrawInToolbar() {
 #if UNITY_2019_3_OR_NEWER
 		if (EditorSettings.enterPlayModeOptionsEnabled) {
 			EnterPlayModeOptions option = EditorSettings.enterPlayModeOptions;
-			selectedEnterPlayMode = (int)option + 1;
+			selectedEnterPlayMode = (int)option;
 		}
 
 		selectedEnterPlayMode = EditorGUILayout.Popup(selectedEnterPlayMode, enterPlayModeOption, GUILayout.Width(WidthInToolbar));
 
-		if (GUI.changed && 0 <= selectedEnterPlayMode && selectedEnterPlayMode < enterPlayModeOption.Length) {
+		if (GUI.changed && 0 <= selectedEnterPlayMode && selectedEnterPlayMode < enterPlayModeOption.Length)
+		{
+			// if (selectedEnterPlayMode == 0) selectedEnterPlayMode = 1;
 			EditorSettings.enterPlayModeOptionsEnabled = selectedEnterPlayMode != 0;
-			EditorSettings.enterPlayModeOptions = (EnterPlayModeOptions)(selectedEnterPlayMode - 1);
+			EditorSettings.enterPlayModeOptions = (EnterPlayModeOptions)(selectedEnterPlayMode);
 		}
 #endif
-	}
+    }
 }
